297ce940fed8aced20b94ba88a63f56501f9d233
liuss7 <976344695@qq.com>
2024-11-25 12:06:48 +0800

    cli 包、create 包初始化

---
+++ packages/create/src/index.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: liuss7 976344695@qq.com
+ * @Date: 2024-11-25 11:58:47
+ * @LastEditors: liuss7 976344695@qq.com
+ * @LastEditTime: 2024-11-25 11:58:55
+ * @FilePath: /xix-cli/packages/create/src/index.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AEra
+ */
+async function create() {
+  console.log('create 命令执行中...')    
+}
+
+export default create;
